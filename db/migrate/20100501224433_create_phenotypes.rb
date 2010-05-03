161f6db2a14a324592beacd29e33204a7f58676a
John Woods <john.woods@marcottelab.org>
2010-05-03 16:35:56 -0500

    Made column_id unique and added an index

--- db/migrate/20100501224433_create_phenotypes.rb
+++ db/migrate/20100501224433_create_phenotypes.rb
@@ -5,11 +5,12 @@ class CreatePhenotypes < ActiveRecord::Migration
       t.string :original_id, :limit => 20, :unique => true
       t.text :desc
       t.integer :genes_count, :default => 0
-      t.integer :column_id
+      t.integer :column_id, :unique => true
     end
 
     add_index :phenotypes, :original_id, :unique => true
     add_index :phenotypes, :species
+    add_index :phenotypes, :column_id, :unique => true
   end
 
   def self.down
