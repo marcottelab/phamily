7714b8d6f7f724598ec963c9874a8291b1d48602
John Woods <john.woods@marcottelab.org>
2010-05-05 19:31:59 -0500

    Started to implement bitvector from ferret

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,6 +1,14 @@
 class Assignment < ActiveRecord::Base
   belongs_to :gene
 
-  named_scope :by_superfamily_id, lambda { |sid| { :conditions => ['superfamily_id = ?', sid]}}
-  default_scope :order => 'e_value'
+  named_scope :by_superfamily_id, lambda { |sid| { :conditions => ['superfamily_id = ?', sid], :order => 'e_value'}}
+
+  # Generate a hash from superfamily_id to bitvector index
+  def self.bitvector_hash
+    @@bitvector_hash ||= Assignment.bitvector_hash_internal
+  end
+protected
+  def self.bitvector_hash_internal
+    
+  end
 end
